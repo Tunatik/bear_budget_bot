6b84d649f5d3a2d29b80918285f949f329244620
Tunatik <fevral-10@yandex.ru>
2025-01-12 17:13:17 +0700

    create class Settings

---
+++ congif.py
@@ -0,0 +1,30 @@
+import os
+
+from loguru import logger
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.default import DefaultBotProperties
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """Project settiings"""
+
+    BOT_TOKEN: str
+    FORMAT_LOG: str = "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}"
+    LOG_ROTATION: str = '10 MB'
+    DB_URL: str = "sqlite+aiosqlite:///db.sqlite3"
+
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
+    )
+
+settings = Settings()
+
+bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage)
+
+log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log.txt")
+logger.add(log_file_path, format=settings.FORMAT_LOG, rotation=settings.LOG_ROTATION, level='INFO')
+database_url = settings.DB_URL
